453bbca80927f1dec38d06182f2df0ad9b11cd87
Trevor Dunn <20tdunn@gmail.com>
2024-02-28 19:31:08 -0600

    Filled out license and added basics to main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+        Created by: Trevor Dunn and Sameer Merchant
+        Creation Date: February 28, 2024
+        Project Name: Movie Rental Database
+        Purpose: Create a database of movies to help ensure that we have a firm
+        grasp on classes for upcoming labs and projects in Programming
+        Fundamentals II
+
+*/
+
+#include "movie.h"
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+int main() {
+  // this is a placeholder comment
+
+  return 0;
+}
